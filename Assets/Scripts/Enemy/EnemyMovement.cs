e34b8aedb979b5e85ec3ceddc0b5a4b9f3e9725d
Fortunatuss <polalex545@gmail.com>
2025-11-15 00:31:42 +1000

    Add Ai

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private Transform[] _points;
+    [SerializeField] private float _speed = 2;
+    [SerializeField] private float _rotationSpeed = 2;
+    private int currentPointIndex = 0;
+
+    void Start()
+    {
+        if (_points.Length > 0)
+        {
+            transform.position = _points[currentPointIndex].position;
+        }
+    }
+
+    void Update()
+    {
+        MoveToNextPoint();
+    }
+
+    private void MoveToNextPoint()
+    {
+        Vector3 direction = _points[currentPointIndex].position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+
+        if (direction.magnitude <= 0.1f)
+        {
+            currentPointIndex++;
+
+            if (currentPointIndex >= _points.Length)
+            {
+                currentPointIndex = 0;
+            }
+        }
+    }
+}
