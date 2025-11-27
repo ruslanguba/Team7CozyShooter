ae3c3e70e26f87de2acba70a1f502050e2338687
Fortunatuss <polalex545@gmail.com>
2025-11-28 00:46:39 +1000

    Change pause menu

--- Assets/Scripts/Ui/Pause.cs
+++ Assets/Scripts/Ui/Pause.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
-    [SerializeField] private InputReader _inputReader;
+    //[SerializeField] private InputReader _inputReader;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenu();
+        }
+    }
 
     public void PauseMenu()
     {
         Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     public void ClosePauseMenu()
     {
         Time.timeScale = 1;
-        _inputReader.enabled = true;
+        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //_inputReader.enabled = true;
+    }
+
+    public void VisibleCursor()
+    {
+        Cursor.visible = true;
     }
 }
