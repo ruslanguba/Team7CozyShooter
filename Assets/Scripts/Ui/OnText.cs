e13f34dc975278efdfadb6c22406e7d58f139b2c
Fortunatuss <polalex545@gmail.com>
2025-12-02 13:59:17 +1000

    Added fade of children in parent object

--- Assets/Scripts/Ui/OnText.cs
+++ Assets/Scripts/Ui/OnText.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,39 +12,89 @@ public class OnText : MonoBehaviour
     private Image text;
     private Color startColor;
 
+    private List<Graphic> graphicsToFade;
+
     private void Awake()
     {
-        text = _object.GetComponent<Image>();
+        graphicsToFade = GetGraphicsFromChildren(_object);
 
-        if (text != null)
+        foreach (Graphic graphic in graphicsToFade)
         {
-            startColor = text.color;
+            graphic.color = new Color(graphic.color.r, graphic.color.g, graphic.color.b, 1f);
         }
+    }
+
+    private List<Graphic> GetGraphicsFromChildren(GameObject root)
+    {
+        var result = new List<Graphic>();
+        foreach (Graphic g in root.GetComponentsInChildren<Graphic>(true))
+        {
+            result.Add(g);
+        }
+        return result;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.TryGetComponent(out PlayerContext playerContext))
+        if (other.gameObject.TryGetComponent<PlayerContext>(out PlayerContext playerContext))
         {
             _object.SetActive(true);
-            StartCoroutine(FadeOut());
+            StartCoroutine(FadeOutAllGraphics());
         }
     }
 
-    private IEnumerator FadeOut()
+    private IEnumerator FadeOutAllGraphics()
     {
         float elapsedTime = 0;
-        yield return new WaitForSeconds(elapsedTime);
         while (elapsedTime < _fadeDuration)
         {
             elapsedTime += Time.deltaTime;
 
-            float alpha = Mathf.Lerp(startColor.a, 0f, elapsedTime / _fadeDuration);
-            text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+            foreach (var graphic in graphicsToFade)
+            {
+                float alpha = Mathf.Lerp(1f, 0f, elapsedTime / _fadeDuration);
+                graphic.color = new Color(graphic.color.r, graphic.color.g, graphic.color.b, alpha);
+            }
 
             yield return null;
         }
-
+        
         _object.SetActive(false);
     }
+
+    //private void Awake()
+    //{
+    //    text = _object.GetComponent<Image>();
+
+    //    if (text != null)
+    //    {
+    //        startColor = text.color;
+    //    }
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.TryGetComponent(out PlayerContext playerContext))
+    //    {
+    //        _object.SetActive(true);
+    //        StartCoroutine(FadeOut());
+    //    }
+    //}
+
+    //private IEnumerator FadeOut()
+    //{
+    //    float elapsedTime = 0;
+    //    yield return new WaitForSeconds(elapsedTime);
+    //    while (elapsedTime < _fadeDuration)
+    //    {
+    //        elapsedTime += Time.deltaTime;
+
+    //        float alpha = Mathf.Lerp(startColor.a, 0f, elapsedTime / _fadeDuration);
+    //        text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+
+    //        yield return null;
+    //    }
+
+    //    _object.SetActive(false);
+    //}
 }
