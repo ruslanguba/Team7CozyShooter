e463a874244eafc39faf8d30f91e2f5c76b18cb0
ruslanguba <122637366+ruslanguba@users.noreply.github.com>
2025-12-04 15:09:24 +0100

    BigCubeDeleated

--- Assets/Project/Ruslan/Scripts/PlatformParenter.cs
+++ Assets/Project/Ruslan/Scripts/PlatformParenter.cs
@@ -6,21 +6,14 @@ public class PlatformParenter : MonoBehaviour
     [SerializeField] private Transform _checkSurfacePivot;
     [SerializeField] private float radius = 0.3f;
 
-    private CharacterController _cc;
     private RotationObject _currentPlatform;
-
-    private void Start()
-    {
-        _cc = _character.GetComponent<CharacterController>();
-    }
 
     private void Update()
     {
         DetectPlatform();
-        MoveWithPlatform();
     }
 
-    void DetectPlatform()
+    public void DetectPlatform()
     {
         Collider[] hits = Physics.OverlapSphere(_checkSurfacePivot.position, radius);
 
@@ -44,30 +37,6 @@ public class PlatformParenter : MonoBehaviour
         else
             _currentPlatform = null;
     }
-
-    void MoveWithPlatform()
-    {
-        if (_currentPlatform == null)
-            return;
-
-        // позиция игрока локально относительно платформы
-        Vector3 local = _currentPlatform.transform.InverseTransformPoint(_character.position);
-
-        // применяем вращение
-        local = _currentPlatform.DeltaRot * local;
-
-        // возвращаем в мировые координаты
-        Vector3 newWorldPos = _currentPlatform.transform.TransformPoint(local);
-
-        // CharacterController не даёт менять transform, поэтому его нужно отключить
-        _cc.enabled = false;
-        _character.position = newWorldPos;
-        _cc.enabled = true;
-
-        // поворачиваем игрока вместе с платформой
-        _character.rotation = _currentPlatform.DeltaRot * _character.rotation;
-    }
-
     public bool IsOnPlatform()
     {
         return _currentPlatform != null;
